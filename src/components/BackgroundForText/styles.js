3ee68f97c1bb712a6a4e7ee6ac97c45880db5611
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-07-31 15:56:31 +0300

    added story Button

---
+++ src/components/BackgroundForText/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+  container: {
+    width: 320,
+    height: 225,
+    backgroundColor: '#F8F8F9',
+    borderRadius: 20,
+    justifyContent: 'center'
+  },
+  titleStyle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#000000',
+  },
+});
---
+++ storybook/storyLoader.js
@@ -0,0 +1,21 @@
+// Auto-generated file created by react-native-storybook-loader
+// Do not edit.
+//
+// https://github.com/elderfo/react-native-storybook-loader.git
+
+function loadStories() {
+	require('../src/components/BackgroundForText/index.stories');
+	require('../src/components/Button/index.stories');
+	require('../src/components/ImageButton/index.stories');
+}
+
+const stories = [
+	'../src/components/BackgroundForText/index.stories',
+	'../src/components/Button/index.stories',
+	'../src/components/ImageButton/index.stories'
+];
+
+module.exports = {
+  loadStories,
+  stories,
+};
