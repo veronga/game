5415a09b17965d61f22eb116a1264550dcd6522f
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-07-31 15:57:51 +0300

    added story BackgroundForText

---
+++ src/components/Button/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native';
+
+
+export default StyleSheet.create({
+  container: {
+    width: 210,
+    height: 80,
+    backgroundColor: '#623F9C',
+    borderRadius: 35,
+    justifyContent: 'center'
+  },
+  titleStyle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#FFFFFF',
+  },
+});
