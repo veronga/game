ab6cf17943194764e03e9d1120e599f48eb9eea6
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-08-02 10:58:24 +0300

    fix

--- src/components/ImageButton/index.stories.js
+++ src/components/ImageButton/index.stories.js
@@ -3,14 +3,13 @@ import { storiesOf } from '@storybook/react-native';
 
 import ImageButton from './index'
 
-import Minus from '../../assets/images/Minus.png'
-import Plus from '../../assets/images/Plus.png'
+
 
 
 storiesOf('ImageButton', module)
-.add('minus', () => (
-  <ImageButton source={Minus}/>
-))
-.add('plus', () => (
-    <ImageButton source={Plus}/>
-  ));
+    .add('minus', () => (
+        <ImageButton imagePath={require('../../assets/images/Minus.png')} />
+    ))
+    .add('plus', () => (
+        <ImageButton imagePath={require('../../assets/images/Plus.png')} />
+    ));
