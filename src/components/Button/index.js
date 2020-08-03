e97bfc97bf024e6ac9a877cbb7c87c325cb9d276
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-08-03 12:04:13 +0300

    fix props

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -1,16 +1,21 @@
 import React from 'react';
-import {  TouchableOpacity, Text } from "react-native";
+import {TouchableOpacity, Text} from 'react-native';
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
+export default function Button({title, onPress}) {
+  const {container, titleStyle} = styles;
 
-export default function Button(title,onPress) {
-    const { container,titleStyle} = styles;
-    
   return (
     <TouchableOpacity style={container} onPress={onPress}>
-       <Text style={titleStyle}>{title}</Text>
+      <Text style={titleStyle}>{title}</Text>
     </TouchableOpacity>
-  )}
+  );
+}
 
- 
+Button.propTypes = {
+  title: PropTypes.string,
+  onPress: PropTypes.func
+};
+
--- src/components/ImageButton/index.js
+++ src/components/ImageButton/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { TouchableOpacity, Image } from "react-native";
+import PropTypes from 'prop-types';
 
 import styles from './styles';
 
 
-export default function Button( onPress, imagePath) {
+export default function Button( {onPress, imagePath}) {
     const { container, imgStyles } = styles;
 
     return (
@@ -16,3 +17,9 @@ export default function Button( onPress, imagePath) {
         </TouchableOpacity>
     )
 }
+
+Button.propTypes = {
+    imagePath: PropTypes.string,
+    onPress: PropTypes.func
+  };
+  
