379c82f9a802b4a3751576841478100469b57a54
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-08-07 16:27:35 +0300

    fix styles

--- src/components/Modal/styles.js
+++ src/components/Modal/styles.js
@@ -1,39 +1,28 @@
 import {StyleSheet} from 'react-native';
 
 export default StyleSheet.create({
-  firstContainer: {
+  container: {
     width: '75%',
     height: 60,
     backgroundColor: '#E569C0',
+    borderColor: '#FFA8F2',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+
+  firstContainer: {
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    borderColor: '#FFA8F2',
-    borderBottomWidth:1,
-    justifyContent: 'center',
   },
-  lastContainer:{
-    width: '75%',
-    height: 60,
-    backgroundColor: '#E569C0',
-    borderColor: '#FFA8F2',
+  lastContainer: {
     borderBottomLeftRadius: 20,
     borderBottomRightRadius: 20,
-    justifyContent: 'center',
   },
-  container:{
-    width: '75%',
-    height: 60,
-    backgroundColor: '#E569C0',
-    borderColor: '#FFA8F2',
-    borderBottomWidth:1,
-    justifyContent: 'center',
-  },
-  stylesText:{
+  stylesText: {
     fontSize: 24,
     fontWeight: 'bold',
     alignItems: 'center',
     textAlign: 'center',
     color: '#fff',
-  }
-
+  },
 });
