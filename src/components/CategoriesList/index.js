2c134691f2119b5995b14898c4f3e521b9f49e4d
Veronika Belozechko <wanheda@MacBook-Pro.local>
2020-08-13 11:43:17 +0300

    added styles

---
+++ src/components/CategoriesList/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+import PropTypes from 'prop-types';
+
+import styles from './styles';
+import categories from '../../categories.json';
+
+export default function CategoriesList() {
+  const {container, stylesText, borderTop, borderBottom} = styles;
+
+  return categories.map((category, index, arr) => {
+    const borberStyle =
+      index === 0 ? borderTop : index === arr.length - 1 ? borderBottom : null;
+    return (
+      <TouchableOpacity key={category}>
+        <View style={[container, borberStyle]}>
+          <Text style={stylesText}>{category}</Text>
+        </View>
+      </TouchableOpacity>
+    );
+  });
+}
+
+CategoriesList.propTypes = {
+  category: PropTypes.string,
+};
---
+++ src/components/CategoriesList/styles.js
@@ -0,0 +1,29 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    width: '75%',
+    height: 60,
+    backgroundColor: '#E569C0',
+    borderColor: '#FFA8F2',
+    borderBottomWidth: 1,
+    justifyContent: 'center',
+  },
+
+  borderTop: {
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+   
+  },
+  borderBottom: {
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
+  },
+  stylesText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    alignItems: 'center',
+    textAlign: 'center',
+    color: '#fff',
+  },
+});
